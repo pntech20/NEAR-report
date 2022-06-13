a5550d68769f3dccc7f597fc7c52f28349153d34
mac <mac@macs-MacBook-Pro.local>
2022-06-13 20:54:13 +0700

    clean code

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,11 +1,3 @@
-/*
- * Example smart contract written in RUST
- *
- * Learn more about writing NEAR smart contracts with Rust:
- * https://near-docs.io/develop/Contract
- *
- */
-
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::UnorderedMap;
 use near_sdk::serde::{Deserialize, Serialize};
@@ -48,18 +40,6 @@ impl Default for Contract {
 // Implement the contract structure
 #[near_bindgen]
 impl Contract {
-    // Public method - returns the greeting saved, defaulting to DEFAULT_MESSAGE
-    pub fn get_greeting(&self) -> String {
-        return self.message.clone();
-    }
-
-    // Public method - accepts a greeting, such as "howdy", and records it
-    pub fn set_greeting(&mut self, message: String) {
-        // Use env::log to record logs permanently to the blockchain!
-        log!("Saving greeting {}", message);
-        self.message = message;
-    }
-
     // add a report
     pub fn add_report(
         &mut self,
@@ -104,8 +84,6 @@ impl Contract {
         };
 
         self.reports.insert(&id, &report);
-        // self.reports.remove(&id);
-        // self.reports.insert(&id, &report);
     }
 
     // delete a report
@@ -125,20 +103,6 @@ impl Contract {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn get_default_greeting() {
-        let contract = Contract::default();
-        // this test did not call set_greeting so should return the default "Hello" greeting
-        assert_eq!(contract.get_greeting(), "Hello".to_string());
-    }
-
-    #[test]
-    fn set_then_get_greeting() {
-        let mut contract = Contract::default();
-        contract.set_greeting("howdy".to_string());
-        assert_eq!(contract.get_greeting(), "howdy".to_string());
-    }
 
     #[test]
     fn add_report() {
